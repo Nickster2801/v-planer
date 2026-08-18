2cc4b5c54de57e2a83df63cb8f5f28bec9b4e0d0
Nickster2801 <stricki2801@gmx.de>
2026-08-18 14:47:15 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.VP_CONFIG = {
-  GOOGLE_CLIENT_ID: "",
+  GOOGLE_CLIENT_ID: "94078554908-0s0v5qbjdj1o2rrs5k2kf0ot2stdmrfj.apps.googleusercontent.com",
   APP_NAME: "V-Planer",
   ROOT_FOLDER_NAME: "Vereinsplanung",
   DEFAULT_STORAGE_LIMIT_GB: 5,
