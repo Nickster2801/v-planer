9095ee9b85efc687ebede685950b3532a45ab8c0
Nickster2801 <stricki2801@gmx.de>
2026-08-21 11:41:25 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.VP_CONFIG = {
-  GOOGLE_CLIENT_ID: "",
+  GOOGLE_CLIENT_ID: "94078554908-0s0v5qbjdj1o2rrs5k2kf0ot2stdmrfj.apps.googleusercontent.com",
   APP_NAME: "V-Planer",
   AUTO_SYNC_SECONDS: 30
 };
